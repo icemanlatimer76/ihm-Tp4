6a25e249c5a852e83d789de1c652eb28e0f60cdf
icemanlatimer76 <31551262+icemanlatimer76@users.noreply.github.com>
2019-02-04 16:35:11 +0100

    Update GereB2.java

--- GereB2.java
+++ GereB2.java
@@ -1,14 +1,13 @@
 import java.awt.event.*;
 
-public class GereB2 implements ActionListener
-{
-	private int nbBtn2;
+public class GereB2 implements ActionListener {
+    private int nbBtn2;
 
-    public void actionPerformed(ActionEvent e)
-    {
-    	nbBtn2++;
-        System.out.println("Bouton B2  : "+ nbBtn2 + "fois");
-        //System.out.println( "  e.getActionCommand() : " + e.getActionCommand() );  
-        //System.out.println( "  e.paramString     () : " + e.paramString     () );        
+    public void actionPerformed(ActionEvent e) {
+        int bite;
+        nbBtn2++;
+        System.out.println("Bouton B2  : " + nbBtn2 + "fois");
+        // System.out.println( " e.getActionCommand() : " + e.getActionCommand() );
+        // System.out.println( " e.paramString () : " + e.paramString () );
     }
 }
